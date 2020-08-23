0babea4d5ddba7d04f38b75e8576db8448c7af6e
kenny <fkennyf@gmail.com>
2020-08-23 03:45:12 +0100

    created privacy policy page

--- routes.js
+++ routes.js
@@ -2,8 +2,12 @@ const express = require("express"),
         router = express.Router();
 
 
-router.get("/", (req,res)=>{
+router.get("/", (req, res)=>{
     res.render("home")
+})
+
+router.get("/privacy-policy", (req, res)=>{
+    res.render("privacy-policy")
 })
 
 
