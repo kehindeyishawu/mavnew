5d366017f8b551bfdab8e68c98eaba95469cb794
kenny <fkennyf@gmail.com>
2020-08-31 03:35:01 +0100

    finished tab layout

--- public/js/list-review.js
+++ public/js/list-review.js
@@ -3,11 +3,11 @@ const collapse = document.querySelector("#collapseExample")
 const hideContent = document.querySelector(".page-content")
 
 
-pageContent.addEventListener("mouseup", ()=>{
-    if(collapse.classList.length === 1){
+pageContent.addEventListener("click", ()=>{
+    if(collapse.classList.length === 2){
         hideContent.classList.add("page-content-hide")
     }else{
         hideContent.classList.remove("page-content-hide")
     }
-    console.log(collapse.classList.length)
+    console.log(collapse.classList.length, collapse.classList)
 })
