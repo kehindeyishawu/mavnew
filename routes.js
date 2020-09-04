5a4bcab00a6c2c26acec0c41be4279261cd3d5c4
kenny <fkennyf@gmail.com>
2020-09-04 04:12:51 +0100

    added semantic tags and recent posts

--- routes.js
+++ routes.js
@@ -15,20 +15,35 @@ router.get("/affiliate-disclosure", (req, res)=>{
     res.render("affiliate-disclosure")
 })
 
-// router.get("/:category/:url", (req, res){
-//     Post.find({url:req.params.url})
-//     res.render(req.params.category)
-// })
-
-router.get("/list-review/:url", (req, res)=>{
-    Post.findOne({url: req.params.url}, (err, post) => {
-        if(err || !post){
-            console.log(err, post)
+router.get("/:category/:url", (req, res) => {
+    let otherPost = []
+    let pagePost;
+    Post.find({}, (err, allPost) => {
+        if(err || !allPost){
+            console.log(err, allPost)
             return res.redirect("/page-not-found")
         }
-        res.render("list-review", {post: post})
+        allPost.forEach(e => {
+            if(e.category === req.params.category && e.url === req.params.url){
+                pagePost = e
+            }else{
+                otherPost.push(e)
+            }
+        })
+        res.locals.recent = otherPost.reverse()
+        res.render(req.params.category, {post: pagePost})
     })
 })
+
+// router.get("/list-review/:url", (req, res)=>{
+//     Post.findOne({url: req.params.url}, (err, post) => {
+//         if(err || !post){
+//             console.log(err, post)
+//             return res.redirect("/page-not-found")
+//         }
+//         res.render("list-review", {post: post})
+//     })
+// })
 
 router.get("/page-not-found", (req, res) => {
     res.send("page could not be found")
