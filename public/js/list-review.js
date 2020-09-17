c3fc69066bf9b30b3ebe5a0ada1e9f8eec51163d
kenny <fkennyf@gmail.com>
2020-09-17 18:39:10 +0100

    reshape social icons

--- public/js/list-review.js
+++ public/js/list-review.js
@@ -1,13 +1,15 @@
-const pageContent = document.querySelector("#page-content")
-const collapse = document.querySelector("#collapseExample")
-const hideContent = document.querySelector(".page-content")
+const pageContent = document.querySelector("#page-content");
+const collapse = document.querySelector("#collapseExample");
+const hideContent = document.querySelector(".page-content");
+// const icons = document.querySelector()
 
+const observer = new IntersectionObserver((entries, observer) => {}, {});
 
-pageContent.addEventListener("click", ()=>{
-    if(collapse.classList.length === 2){
-        hideContent.classList.add("page-content-hide")
-    }else{
-        hideContent.classList.remove("page-content-hide")
-    }
-    console.log(collapse.classList.length, collapse.classList)
-})
+pageContent.addEventListener("click", () => {
+  if (collapse.classList.length === 2) {
+    hideContent.classList.add("page-content-hide");
+  } else {
+    hideContent.classList.remove("page-content-hide");
+  }
+  console.log(collapse.classList.length, collapse.classList);
+});
