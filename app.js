5d6c295c06281101e8cee8ac88f12669cd181b2d
kenny <fkennyf@gmail.com>
2020-10-12 04:24:42 +0100

    added http-https redirect4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   if (req.protocol !== "https" && process.env.NODE_ENV === "prod") {
     return res.redirect(
       301,
-      `https://mavnew.herokuapp.com${req.baseUrl + req.path}`
+      `https://mavnew.herokuapp.com${req.headers.host + req.url}`
     );
   }
   next();
