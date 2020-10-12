0b1ad28e958a5e32b6c1933d6792b5647dc1e15e
kenny <fkennyf@gmail.com>
2020-10-12 04:40:15 +0100

    https attempt hopeful this time

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
   if (process.env.NODE_ENV === "prod") {
-    if (req.headers.host === "www.mavnew.com")
-      return res.redirect(301, "https://mavnew.herokuapp.com" + req.url);
+    // if (req.headers.host === "www.mavnew.com")
+    //   return res.redirect(301, "https://mavnew.herokuapp.com");
     if (req.headers["x-forwarded-proto"] !== "https")
       return res.redirect("https://" + req.headers.host + req.url);
     else return next();
